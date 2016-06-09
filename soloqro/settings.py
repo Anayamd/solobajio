049212375bf8e96e0308ab7dfec58fc49a926c4b
Diego Mancilla <anayamd@hotmail.com>
2016-06-09 10:40:17 -0500

    Debug True

--- soloqro/settings.py
+++ soloqro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3z%t(fu$1dodvr(v78x*%7^$pugv+spyb!i02kyi947%5eis1&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
