a4393e99a90f1c51a2753514e795814a7a804176
Diego Mancilla <anayamd@hotmail.com>
2016-06-09 08:58:06 -0500

    Edited settings to work with heroku

--- soloqro/settings.py
+++ soloqro/settings.py
@@ -122,13 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
-
+STATIC_ROOT = 'staticfiles'
+STATIC_URL ='/static/'
+MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
+MEDIA_URL = "/media/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'soloqro', 'static'),
+os.path.join(BASE_DIR, 'static'),
 )
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
