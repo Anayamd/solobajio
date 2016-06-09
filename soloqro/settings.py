65d713e271c521b722f2082c575dc4d74ee99cde
Diego Mancilla <anayamd@hotmail.com>
2016-06-09 10:13:37 -0500

    Settings 5.0

--- soloqro/settings.py
+++ soloqro/settings.py
@@ -137,5 +137,5 @@ STATIC_URL ='/staticfiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'soloqro', 'static'),
+os.path.join(BASE_DIR, 'spv', 'static'),
 )
